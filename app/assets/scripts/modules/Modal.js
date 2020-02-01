87d0e501be170c559eac7edf64f9073384d30252
dor.koren <dor.koren@post.idc.ac.il>
2020-02-01 19:05:58 +0200

    Copleted the modal overlay

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,15 +3,10 @@ class Modal {
         this.injectHTML();
         this.modal = document.querySelector(".modal");
         this.closeIcon = document.querySelector(".modal__close");
-        this.openModalButtons = document.querySelectorAll(".open-modal");
         this.events();
-
     }
 
     events() {
-        // listen for open click
-        this.openModalButtons.forEach(element => element.addEventListener("click", event => this.openTheModal(event)));
-
         // listen for close click
         this.closeIcon.addEventListener("click", () => this.closeTheModal());
 
@@ -25,8 +20,7 @@ class Modal {
         }
     }
 
-    openTheModal(event) {
-        event.preventDefault();
+    openTheModal() {
         this.modal.classList.add("modal--is-visible");
     }
 
